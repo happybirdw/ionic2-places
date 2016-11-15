ab348984918ece74081a463f2a22db9b66167951
Richardson Santos <rpsantosvix@gmail.com>
2016-11-15 10:39:44 +0100

    mystep1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PlacesPage } from '../pages/places/places';
+import { AddPage } from '../pages/add/add';
+import { AroundPage } from '../pages/around/around';
+import { FriendsPage } from '../pages/friends/friends';
+import { TabsPage } from '../pages/tabs/tabs';
+
+const pages = [
+    MyApp,
+    HomePage,
+    PlacesPage,
+    AddPage,
+    AroundPage,
+    FriendsPage,
+    TabsPage,
+]
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: pages,
   imports: [
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: pages,
   providers: []
 })
 export class AppModule {}
